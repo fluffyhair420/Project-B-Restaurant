3a30570fb640b3d01f094241e17179cf65794450
Lorenzohro <99186287+Lorenzohro@users.noreply.github.com>
2023-04-18 13:25:07 +0200

    Add files via upload

---
+++ DataAccess/ReviewToJson.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+
+public class ReviewToJson
+{
+    public void Start()
+    {
+        SerializeToJson();
+    }
+
+    void SerializeToJson()
+    {
+        var p = new Product("Product A", new DateTime(2021, 12, 28),
+        new string[] { "small" });
+
+        var json = JsonConvert.SerializeObject(p);
+        Console.WriteLine(json);
+
+        record Product(string Name, DateTime Created, string[] Sizes);
+
+    }
+}
